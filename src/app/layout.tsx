723fa9dc859d2b426bba793323073f2968741dc9
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-08 16:57:22 -0300

    (chore) update packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from 'react';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'My Wishlist',
+  title: 'Wishlist',
   description: 'Wishlist for study purposes',
 };
 
