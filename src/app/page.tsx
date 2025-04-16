c086ecf55895c3e840a64ca2fa3fcbedfc5b9331
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-16 15:58:50 -0300

    (feat) set design tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,10 @@
 
 import { useState } from 'react';
 
-import { Breadcrumb } from './components/Breadcrumb';
-import { Header } from './components/Header';
 import { ProductCard } from './components/ProductCard';
 import { WishlistButton } from './components/WishlistButton';
 
 export default function Home() {
-  const breadcrumbLinks = [
-    {
-      label: 'Home',
-      href: '/',
-      title: 'Home',
-    },
-  ];
-
   const [wishlistButtonState, setWishlistButtonState] = useState<'active' | null>(null);
 
   const onWishlistButtonClick = () => {
@@ -24,21 +14,17 @@ export default function Home() {
 
   return (
     <div>
-      <Header />
-      <main>
-        <Breadcrumb links={breadcrumbLinks} />
-        <ProductCard
-          imageSrc="https://static.netshoes.com.br/produtos/tenis-nike-revolution-7-feminino/26/JD8-6363-026/JD8-6363-026_zoom1.jpg?ts=1708096757?ims=544x"
-          title="Tênis Nike Revolution 7 Feminino - Preto+Branco"
-          fullPrice="39999"
-          salePrice="30399"
-        >
-          <WishlistButton
-            onClick={onWishlistButtonClick}
-            state={wishlistButtonState}
-          />
-        </ProductCard>
-      </main>
+      <ProductCard
+        imageSrc="https://static.netshoes.com.br/produtos/tenis-nike-revolution-7-feminino/26/JD8-6363-026/JD8-6363-026_zoom1.jpg?ts=1708096757?ims=544x"
+        title="Tênis Nike Revolution 7 Feminino - Preto+Branco"
+        fullPrice="39999"
+        salePrice="30399"
+      >
+        <WishlistButton
+          onClick={onWishlistButtonClick}
+          state={wishlistButtonState}
+        />
+      </ProductCard>
     </div>
   );
 }
