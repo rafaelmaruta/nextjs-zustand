7d6cb16dd83acdefed874ae441a27aeb00258aad
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-16 15:58:50 -0300

    (feat) set design tokens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 import { Source_Sans_3 } from 'next/font/google';
 import type { ReactNode } from 'react';
 
+import { Breadcrumb } from './components/Breadcrumb';
+import { Header } from './components/Header';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -21,12 +23,26 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const breadcrumbLinks = [
+    {
+      label: 'Home',
+      href: '/',
+      title: 'Home',
+    },
+  ];
+
   return (
     <html
       lang="en"
       className={SourceSans3.variable}
     >
-      <body className="font-sans">{children}</body>
+      <body className="font-sans">
+        <Header />
+        <main>
+          <Breadcrumb links={breadcrumbLinks} />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
