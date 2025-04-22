e23029a90580f98bf941f98a3130256c695ac654
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-22 18:33:30 -0300

    (feat) menu desk styling

--- src/app/components/NavigationMenu/index.tsx
+++ src/app/components/NavigationMenu/index.tsx
@@ -22,22 +22,25 @@ export const NavigationMenu = ({ visibility = 'visible' }: NavigationMenuProps) 
     },
   ];
 
+  const arrow =
+    "md:absolute md:before:content-[''] md:before:absolute md:before:right-0 md:before:-translate-x-1/2 md:before:border-l-[12px] md:before:border-r-[12px] md:before:top-[-12px] md:before:border-b-[12px] md:before:border-l-transparent md:before:border-r-transparent md:before:border-b-white";
+
   return (
     <menu
-      className={`${visibility} absolute h-full bg-[var(--background)] pr-[var(--content-spacing)] shadow-[3px_0_4px_rgba(0,0,0,0.05)]`}
+      className={`${visibility} absolute h-full bg-[var(--background)] pr-[var(--content-spacing)] shadow-[3px_0_4px_var(--menu-shadow)] md:top-[-12px] md:right-[var(--content-spacing)] md:mr-[-8px] md:h-auto md:rounded-[8px] md:pt-[5px] md:pr-[10px] md:pb-[5px] md:pl-[10px] md:shadow-[0_4px_4px_var(--desk-menu-shadow)] ${arrow}`}
     >
-      <ul className="flex h-full flex-col">
-        {menuOptions.map((option) => (
+      <ul className="flex h-full flex-col md:h-auto">
+        {menuOptions.map(({ href, text }) => (
           <li
-            key={option.href}
-            className="border-b-[1px] border-[var(--border-color-100)]"
+            key={href}
+            className="border-b-[1px] border-[var(--border-color-100)] md:border-0"
           >
             <Link
-              className="block pt-[10px] pb-[10px]"
-              href={option.href}
-              title={option.text}
+              className="block pt-[10px] pb-[10px] text-[length:var(--font-size-3)] text-[var(--gray-200)] md:pt-[5px] md:pb-[5px] md:hover:underline"
+              href={href}
+              title={text}
             >
-              {option.text}
+              {text}
             </Link>
           </li>
         ))}
