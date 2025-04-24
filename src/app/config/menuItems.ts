e3e5a6b0c83d93175274da67d6e9d021d4242f1a
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-24 18:15:11 -0300

    (refac) menu items in a config file and breadcrumb component in each page

---
+++ src/app/config/menuItems.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    href: '/login',
+    text: 'Entrar',
+  },
+  {
+    href: '/my-account',
+    text: 'Minha Conta',
+  },
+  {
+    href: '/address',
+    text: 'Endereços',
+  },
+  {
+    href: '/my-netshoes',
+    text: 'Minha Netshoes',
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { Breadcrumb } from './components/Breadcrumb';
 import { ProductCard } from './components/ProductCard';
 import { WishlistButton } from './components/WishlistButton';
 
@@ -12,19 +13,31 @@ export default function Home() {
     setWishlistButtonState('active');
   };
 
+  const breadcrumbLinks = [
+    {
+      label: 'Home',
+      href: '/',
+      title: 'Home',
+      className: '',
+    },
+  ];
+
   return (
-    <div>
-      <ProductCard
-        imageSrc="https://static.netshoes.com.br/produtos/tenis-nike-revolution-7-feminino/26/JD8-6363-026/JD8-6363-026_zoom1.jpg?ts=1708096757?ims=544x"
-        title="Tênis Nike Revolution 7 Feminino - Preto+Branco"
-        fullPrice="39999"
-        salePrice="30399"
-      >
-        <WishlistButton
-          onClick={onWishlistButtonClick}
-          state={wishlistButtonState}
-        />
-      </ProductCard>
-    </div>
+    <>
+      <Breadcrumb links={breadcrumbLinks} />
+      <section>
+        <ProductCard
+          imageSrc="https://static.netshoes.com.br/produtos/tenis-nike-revolution-7-feminino/26/JD8-6363-026/JD8-6363-026_zoom1.jpg?ts=1708096757?ims=544x"
+          title="Tênis Nike Revolution 7 Feminino - Preto+Branco"
+          fullPrice="39999"
+          salePrice="30399"
+        >
+          <WishlistButton
+            onClick={onWishlistButtonClick}
+            state={wishlistButtonState}
+          />
+        </ProductCard>
+      </section>
+    </>
   );
 }
