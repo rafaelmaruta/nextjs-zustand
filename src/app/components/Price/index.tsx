e47cf87e3e38a98d6bce2ab17bf715589e4eeb47
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-28 18:46:24 -0300

    (chore) ProductCard component styling

--- src/app/components/Price/index.tsx
+++ src/app/components/Price/index.tsx
@@ -3,8 +3,12 @@ import { PriceProps } from './types';
 export const Price = ({ fullPrice, salePrice }: PriceProps) => {
   return (
     <div>
-      <p>{fullPrice}</p>
-      <p>{salePrice}</p>
+      <p className="text-[length:var(--font-size-2)] text-[var(--font-color-100)] line-through">
+        {fullPrice}
+      </p>
+      <p className="text-[length:var(--font-size-5)] leading-none font-bold text-[var(--purple-200)]">
+        {salePrice}
+      </p>
     </div>
   );
 };
