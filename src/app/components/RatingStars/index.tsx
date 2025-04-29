eaa6a9872a096656945004bdee9a9e73d807a258
Rafael Maruta <maruta.rafael@gmail.com>
2025-04-29 16:38:06 -0300

    (fix) adjust ProductCard component styling

--- src/app/components/RatingStars/index.tsx
+++ src/app/components/RatingStars/index.tsx
@@ -6,11 +6,12 @@ import { RatingStarsProps } from './types';
 
 export const RatingStars = ({ rating = 0 }: RatingStarsProps) => {
   return (
-    <div>
+    <div className="flex items-center">
       <Image
         src={Star}
         alt={`${rating}`}
       />
+      <span className="ml-[5px] text-[length:var(--font-size-1)]">5.0</span>
     </div>
   );
 };
