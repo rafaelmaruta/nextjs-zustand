e26874c90c97a5d59b34295d3034ec07698d1aaf
Rafael Maruta <maruta.rafael@gmail.com>
2025-05-01 16:38:41 -0300

    (refac) RatingStars component improvements, refac project consts

--- src/app/components/NavigationMenu/index.tsx
+++ src/app/components/NavigationMenu/index.tsx
@@ -5,6 +5,9 @@ import { menuItems } from '@/app/config/menuItems';
 
 import { NavigationMenuProps } from './types';
 
+const ARROW =
+  "md:absolute md:before:content-[''] md:before:absolute md:before:right-0 md:before:-translate-x-1/2 md:before:border-l-[12px] md:before:border-r-[12px] md:before:top-[8px] md:before:border-b-[12px] md:before:border-l-transparent md:before:border-r-transparent md:before:border-b-white";
+
 export const NavigationMenu = ({
   visibility = 'visible',
   onMouseOver,
@@ -13,9 +16,6 @@ export const NavigationMenu = ({
   onBlur,
   onClickOutside,
 }: NavigationMenuProps) => {
-  const arrow =
-    "md:absolute md:before:content-[''] md:before:absolute md:before:right-0 md:before:-translate-x-1/2 md:before:border-l-[12px] md:before:border-r-[12px] md:before:top-[8px] md:before:border-b-[12px] md:before:border-l-transparent md:before:border-r-transparent md:before:border-b-white";
-
   const ref = useRef<HTMLMenuElement>(null);
 
   useEffect(() => {
@@ -41,7 +41,7 @@ export const NavigationMenu = ({
       onMouseOut={onMouseOut}
       onFocus={onFocus}
       onBlur={onBlur}
-      className={`${visibility} absolute h-full md:top-[-32px] md:right-[var(--content-spacing)] md:mr-[-8px] md:h-auto ${arrow}`}
+      className={`${visibility} absolute h-full md:top-[-32px] md:right-[var(--content-spacing)] md:mr-[-8px] md:h-auto ${ARROW}`}
     >
       <ul className="flex h-full flex-col bg-[var(--background)] pr-[var(--content-spacing)] shadow-[3px_0_4px_var(--menu-shadow)] md:mt-[20px] md:h-auto md:rounded-[8px] md:pt-[5px] md:pr-[10px] md:pb-[5px] md:pl-[10px] md:shadow-[0_4px_4px_var(--desk-menu-shadow)]">
         {menuItems.map(({ href, text }) => (
