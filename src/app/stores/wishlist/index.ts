f30607442afb6744d5df53a995aa62a7dfad2f9e
Rafael Maruta <maruta.rafael@gmail.com>
2025-05-12 17:58:53 -0300

    (refac) improve WishlistStore

--- src/app/stores/wishlist/index.ts
+++ src/app/stores/wishlist/index.ts
@@ -1,12 +1,17 @@
 import { create } from 'zustand';
+import { combine, devtools, persist } from 'zustand/middleware';
 
-import { WishlistStore } from './types';
-
-export const useWishlistStore = create<WishlistStore>((set) => ({
-  items: [],
-  addItem: (sku) => set((state) => ({ items: [...state.items, sku] })),
-  removeItem: (sku) =>
-    set((state) => ({
-      items: state.items.filter((item) => item !== sku),
-    })),
-}));
+export const useWishlistStore = create(
+  devtools(
+    persist(
+      combine({ items: [] as string[] }, (set) => ({
+        addItem: (sku: string) => set((state) => ({ items: [...state.items, sku] })),
+        removeItem: (sku: string) =>
+          set((state) => ({
+            items: state.items.filter((item) => item !== sku),
+          })),
+      })),
+      { name: 'wishlistStore' },
+    ),
+  ),
+);
--- src/app/stores/wishlist/types.ts
+++
@@ -1,5 +0,0 @@
-export interface WishlistStore {
-  items: string[];
-  addItem: (sku: string) => void;
-  removeItem: (sku: string) => void;
-}
