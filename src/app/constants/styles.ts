216ce8b460ffa80141093de3793181dc568f6cfb
Rafael Maruta <maruta.rafael@gmail.com>
2025-05-13 11:40:24 -0300

    (feat) wishlist page integration with store

--- src/app/constants/styles.ts
+++ src/app/constants/styles.ts
@@ -1,4 +1,4 @@
 export const OUTER_PAGE_CLASSES = 'flex justify-center md:mt-[var(--content-spacing)]';
 
 export const INNER_PAGE_CLASSES =
-  'flex md:max-w-[960px] flex-wrap justify-center gap-[var(--content-spacing)] md:justify-start mb-[var(--content-spacing)]';
+  'flex md:max-w-[960px] w-[100%] flex-wrap justify-center gap-[var(--content-spacing)] md:justify-start mb-[var(--content-spacing)]';
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,29 +1,50 @@
 'use client';
 
+import { useCallback, useEffect } from 'react';
+
 import { Breadcrumb } from '@/app/components/Breadcrumb';
 import { ProductCard } from '@/app/components/ProductCard';
 import { RemoveButton } from '@/app/components/RemoveButton';
 import { INNER_PAGE_CLASSES, OUTER_PAGE_CLASSES } from '@/app/constants/styles';
+import { useProductsStore } from '@/app/stores/products';
+import { useWishlistStore } from '@/app/stores/wishlist';
 import { centsToReais } from '@/app/utils/centsToReais';
 
 export default function Wishlist() {
-  const onRemoveButtonClick = () => {};
+  const { isInWishlist, removeItem } = useWishlistStore();
+  const { result, getProducts } = useProductsStore();
+
+  const onRemoveButtonClick = useCallback(
+    (code: string) => () => {
+      removeItem(code);
+    },
+    [removeItem],
+  );
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
 
   return (
     <>
       <Breadcrumb />
       <section className={OUTER_PAGE_CLASSES}>
         <div className={INNER_PAGE_CLASSES}>
-          <ProductCard
-            sku="000"
-            imageSrc="https://static.netshoes.com.br/produtos/tenis-nike-revolution-7-feminino/26/JD8-6363-026/JD8-6363-026_zoom1.jpg?ts=1708096757?ims=544x"
-            title="Tênis Nike Revolution 7 Feminino - Preto+Branco"
-            rating={4.2}
-            fullPrice={centsToReais(+'39999')}
-            salePrice={centsToReais(+'30399')}
-          >
-            <RemoveButton onClick={onRemoveButtonClick} />
-          </ProductCard>
+          {result?.products
+            .filter((product) => isInWishlist(product.code))
+            .map((product) => (
+              <ProductCard
+                key={product.code}
+                sku={product.code}
+                imageSrc={product.image}
+                title={product.name}
+                rating={product.rating}
+                fullPrice={centsToReais(+product.fullPriceInCents)}
+                salePrice={centsToReais(+product.salePriceInCents)}
+              >
+                <RemoveButton onClick={onRemoveButtonClick(product.code)} />
+              </ProductCard>
+            ))}
         </div>
       </section>
     </>
